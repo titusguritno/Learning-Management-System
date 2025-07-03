ec87cd157f8b7ed7cad1101f3c0e727050069742
titusguritno <guritnotitus@gmail.com>
2025-07-03 23:43:59 +0700

    Add navbar in layout dashboard

---
+++ components/navbar-routes.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { UserButton } from "@clerk/nextjs";
+import { usePathname } from "next/navigation";
+import { LogOut } from "lucide-react";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+
+export const NavbarRoutes = () => {
+  const pathname = usePathname();
+
+  const isTeacherPage = pathname?.startsWith("/teacher");
+  const isPlayerPage = pathname?.includes("/chapter");
+
+  return (
+    <div className="flex gap-x-2 ml-auto">
+      <UserButton afterSwitchSessionUrl="/" />
+    </div>
+  );
+};
