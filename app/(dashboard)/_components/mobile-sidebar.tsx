683ca8e7a35f14324833ad391fcb6bbffeb29e9c
titusguritno <guritnotitus@gmail.com>
2025-07-04 00:02:00 +0700

    make navbar get the responsif on mobile

---
+++ app/(dashboard)/_components/mobile-sidebar.tsx
@@ -0,0 +1,27 @@
+import { Menu } from "lucide-react";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import { Sidebar } from "./sidebar";
+
+export const MobileSidebar = () => {
+  return (
+    <Sheet>
+      <SheetTrigger className="md:hidden pr-4 hover:opacity-75 transition">
+        <Menu />
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0 bg-white">
+        <SheetHeader>
+          <SheetTitle className="sr-only">Mobile Sidebar</SheetTitle>
+        </SheetHeader>
+
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
+};
